52bbc0376953469c3eeb3991cba8f8afd0734744
shanoukhavi <shanoukh5@gmail.com>
2024-07-01 17:39:19 +0530

    done change

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -3,7 +3,7 @@ import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from 
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
-import { spawn } from "child_process";
+
 import MobileNavLinks from "./MobileNavLinks";
 
 
