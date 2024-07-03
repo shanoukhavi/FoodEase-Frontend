a3798e91993fa2b8d6ecb338d06223608e89302b
shanoukhavi <shanoukh5@gmail.com>
2024-07-03 22:26:34 +0530

    implementation for refreshing done here

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -2,9 +2,17 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute=()=>{
-const {isAuthenticated}=useAuth0();
-return isAuthenticated?(<Outlet/>):(<Navigate to="/" replace/>)
+const {isAuthenticated,isLoading}=useAuth0();
+if(isLoading){
+    return null;
+    //si null uwil lget 
+}
+if(isAuthenticated){
+return <Outlet/>;
+}
+return <Navigate to="/" replace  />;
+// return isAuthenticated?(<Outlet/>):(<Navigate to="/" replace/>)
 }
 
 export default ProtectedRoute;
-// see there are many routes been defined only the loggedin can go forward otherwise it wont be redirected  
+// see there are many routes been defined only the loggedin can go forward otherwise it wont be redirected  here when referesh he should get it 
