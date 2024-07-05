871c451eef4d515f4755ab04841cd0d263a34caa
shanoukhavi <shanoukh5@gmail.com>
2024-07-05 23:01:36 +0530

    search filter added mate

--- src/api/RestaurantApi.tsx
+++ src/api/RestaurantApi.tsx
@@ -1,15 +1,18 @@
+import { SearchState } from "@/pages/SearchPage";
 import { RestaurantSearchResponse } from "@/types";
 import { useQuery } from "react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const useSearchRestaurants=(city?: string)=>{
+export const useSearchRestaurants=(searchState:SearchState,city?: string)=>{
     const createSearchRequest=async():Promise<RestaurantSearchResponse>=>{
-        const response=await fetch(`${API_BASE_URL}/api/restaurant/search/${city }`);
+        const params=new URLSearchParams();
+        params.set("searchQuery",searchState.searchQuery);
+        const response=await fetch(`${API_BASE_URL}/api/restaurant/search/${city }?${params.toString()}`);
     if(!response.ok){
         throw new Error("Failed to fetch restaurants");
     }
     return response.json();
     }
-    const {data:results,isLoading}=useQuery(["searchRestaurants"],createSearchRequest,{enabled:!!city});
+    const {data:results,isLoading}=useQuery(["searchRestaurants",searchState],createSearchRequest,{enabled:!!city});
     return {results, isLoading};
 }
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,11 +1,32 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
+import SearchBar, { SearchForm } from "@/SearchBar";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
-
+export type SearchState={
+  searchQuery:string;
+  //user serching at searchabar mate 
+}
 const SearchPage = () => {
   const { city } = useParams();
-  const { results ,isLoading} = useSearchRestaurants(city);
+  const [searchState,setSearchState] = useState<SearchState>({
+    searchQuery:"",
+    //this is the search query that user is searching for mate
+  })
+  const { results ,isLoading} = useSearchRestaurants(searchState,city);
+  const setSearchQuery = (searchFormData:SearchForm)=>{
+setSearchState((prevState)=>({
+...prevState,
+searchQuery:searchFormData.searchQuery,
+}));
+  };
+  const resetSearch = ()=>{
+    setSearchState((prevState)=>({
+      ...prevState,
+      searchQuery:"",
+      }));
+  }
   if(isLoading) {
 
     //like ur resut been loading then u will get the page 
@@ -19,6 +40,7 @@ const SearchPage = () => {
 <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
     <div id="cuisines-left">insert cuisines here  </div>
     <div id="main-content" className="flex flex-col gap-5">
+      <SearchBar searchQuery={searchState.searchQuery} onSubmit={setSearchQuery} placeHolder="Search by Cuisisne or Restaurant Name" onReset={resetSearch}/>
         <SearchResultInfo total={results.pagination.total} city={city}/>
         {results.data.map((restaurant)=>(
             <SearchResultCard restaurant={restaurant}/>
